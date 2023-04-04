5996463ddb8db5af24daa3f7950f21922ddf61de
Alexander <apvannug.lhl.webdev@gmail.com>
2023-04-04 16:24:24 -0600

    Refactor logic for p1 and p2 turns

--- main.rb
+++ main.rb
@@ -8,8 +8,82 @@ class Main
   @@p2_HP = 3
 
   def initialize
+
+    #Initial turn starts with player 1
+
+    # While p1 and p2 are above 0 HP
+    while @@p1_HP > 0 && @@p2_HP > 0
+
+      question1 = Question.new
+      answer1 = ask_question.question_player1
+      reponse1 = Response.new
+
+      # If the response of answer1 is equal to true (correct), p1 no lives lost
+      if response1.compare(answer1)
+        @@p1_HP
+        puts "P1: #{@@p1_HP}/3 vs P2: #{@@p2_HP}/3"
+
+        # Else answer1 is false (incorrect), p1 lose 1 life
+      else
+        @@p1_HP -= 1
+        puts "P1: #{@@p1_HP}/3 vs P2: #{@@p2_HP}/3"
+      end
+
+      # If p1 health equals to 0
+      if @@p1_HP == 0
+        puts "Player 1 wins with #{@@p1_HP}/3 lives remaining! 🎊"
+        puts "---- GAME OVER ----"
+        puts "Thank you for playing!"
+        break
+      
+      # If p2 health equals to 0
+      elsif @@p2_HP == 0
+        puts "Player 2 wins with #{@@p2_HP}/3 lives remaining! 🎊"
+        puts "---- GAME OVER ----"
+        puts "Thank you for playing!"
+        break
+      end
+
+      # Begin new turn (Player 2)
+      puts "---- NEW TURN ----"
+
+
+      question2 = Question.new
+      answer2 = ask_question.question_player2
+      reponse2 = Response.new
+
+      # If the response of answer2 is equal to true (correct), p2 no lives lost
+      if response2.compare(answer2)
+        @@p2_HP
+        puts "P1: #{@@p1_HP}/3 vs P2: #{@@p2_HP}/3"
+
+        # Else answer2 is false (incorrect), p2 lose 1 life
+      else
+        @@p2_HP -= 1
+        puts "P1: #{@@p1_HP}/3 vs P2: #{@@p2_HP}/3"
+      end
+
+      # If p1 health equals to 0
+      if @@p1_HP == 0
+        puts "Player 1 wins with #{@@p1_HP}/3 lives remaining! 🎊"
+        puts "---- GAME OVER ----"
+        puts "Thank you for playing!"
+        break
+      
+      # If p2 health equals to 0
+      elsif @@p2_HP == 0
+        puts "Player 2 wins with #{@@p2_HP}/3 lives remaining! 🎊"
+        puts "---- GAME OVER ----"
+        puts "Thank you for playing!"
+        break
+      end
+
+
+      puts "---- NEW TURN ----"
+
+    end
   end
-  
-
-
 end
+
+math_game = Main.new
+math_game.start
